8f8872860d4227490f01dba29bd5fb5dcf3df2a2
Guto <guromagnolo23@hotmail.com>
2021-09-04 00:44:15 -0300

    First post of all files

---
+++ src/components/layout/Dashboard.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import CharCard from '../MK11/CharCard';
+
+export default class Dashboard extends Component {
+    render(){
+        return <CharCard />
+    }   
+}
